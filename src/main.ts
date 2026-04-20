b5eec9feb1850575f737d9e6b925cca8af6bae25
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-04-20 11:51:33 +0200

    hämtar data

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
 import './style.css'
 
+async function getData() {
+  const res = await fetch("http://localhost:5001/workexperience");
+  const data = await res.json();
 
+  console.table(data);
+}
+
+getData();

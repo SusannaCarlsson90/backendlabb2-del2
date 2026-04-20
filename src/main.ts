dcfa2fba4cfeafa243bc7314d33884d5f712c93d
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-04-20 11:54:22 +0200

    test funktion skicka data

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,30 @@ async function getData() {
 }
 
 getData();
+
+async function createExperience() {
+  //Skapar objekt
+  let experience = { 
+      companyname: "Mittuniversitetet", 
+      jobtitle: "Student",
+      location: "Sundsvall",
+      startdate: "2023-01-01",
+      enddate: "2024-01-01",
+      description: "Läser webbutveckling."
+  };
+
+  //Skicka till port 5001 och rätt route
+  const res = await fetch("http://localhost:5001/workexperience", {
+      method: "POST",
+      headers: {
+          "content-type": "application/json"
+      },
+      body: JSON.stringify(experience)
+  });
+
+  const data = await res.json();
+  console.log(data);
+}
+
+// Kör funktionen för att skicka datan
+createExperience();

2debf46f2f695a16031338390ffe7dc581d6038b
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-04-22 15:08:39 +0200

    styling klar

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ if (hamburger && navLinks) {
 async function getData() {
   const res = await fetch("http://localhost:5001/workexperience");
   const data = await res.json();
+  console.table(data);
 
   //Hämtar min div från index.html
   const experienceList =  document.getElementById("experience-list");
@@ -27,6 +28,9 @@ async function getData() {
           <p><strong>${work.jobtitle}</strong> - ${work.location}</p>
           <p><em>${work.startdate} till ${work.enddate}</em></p>
           <p>${work.description}</p>
+          <button onclick="deleteExperience(${work.ID})">
+        Radera
+      </button>
         </div>
       `;
     });
@@ -35,7 +39,29 @@ async function getData() {
 
 getData();
 
+(window as any).deleteExperience = async (id: number) => {
+  //Dubbelkolla om användaren vill radera?
+  if (confirm("Är du säker på att du vill radera denna erfarenhet?")) {
+      
+      try {
+          //Skicka DELETE-anropet till servern
+          const res = await fetch(`http://localhost:5001/workexperience/${id}`, {
+              method: "DELETE"
+          });
 
+          if (res.ok) {
+              //Om det gick bra, ladda om listan så att den raderade posten försvinner
+              getData(); 
+          } else {
+              alert("Gick inte att radera från servern.");
+          }
+          
+      } catch (error) {
+        console.error("DETTA ÄR FELET:", error.message); // Lägg till denna rad!
+        res.status(500).json({ message: error.message });
+      }
+  }
+};
 
 
 
